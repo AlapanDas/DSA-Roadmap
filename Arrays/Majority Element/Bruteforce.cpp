8fc7d057dd50a1d2181687d462de60db2258a3f8
Alapan <alapandas333@gmail.com>
2022-04-26 21:23:00 +0530

    Added Two files

---
+++ Arrays/Majority Element/Bruteforce.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    int max = 0;
+    int index = -1; 
+    for (int i = 0; i < n; i++) {
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j])
+                cnt++;
+        }
+        if (cnt > max) {
+            max = cnt;
+            index = i;
+        }
+    }
+    if (max > n / 2)
+        cout << arr[index] << endl;
+    else
+        cout << "None" << endl;
+}
